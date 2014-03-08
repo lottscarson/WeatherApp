c282a25393e4340bebf959574ae209e8200ba1c5
Scott Larson <slarson@ibfx.com>
2014-03-08 10:03:15 -0700

    Added a UINavigationController subclass to prevent autorotation

---
+++ WeatherApp/WANavigationController.h
@@ -0,0 +1,13 @@
+//
+//  WANavigationController.h
+//  WeatherApp
+//
+//  Created by Scott Larson on 3/8/14.
+//  Copyright (c) 2014 SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WANavigationController : UINavigationController
+
+@end
---
+++ WeatherApp/WANavigationController.m
@@ -0,0 +1,22 @@
+//
+//  WANavigationController.m
+//  WeatherApp
+//
+//  Created by Scott Larson on 3/8/14.
+//  Copyright (c) 2014 SL. All rights reserved.
+//
+
+#import "WANavigationController.h"
+
+@interface WANavigationController ()
+
+@end
+
+@implementation WANavigationController
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+@end
